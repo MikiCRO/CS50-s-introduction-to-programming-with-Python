9c564e92a990fbe3a02c56f3c97b8c1c7220b7e3
MikiCRO <135828074+MikiCRO@users.noreply.github.com>
2024-02-05 19:02:31 +0100

    Add files via upload

---
+++ Project set 1/bank.py
@@ -0,0 +1,23 @@
+def main():
+    x=value(str(input("Greetings: "))).lower().strip()
+    print(f"${x}")
+
+
+def value(greeting):
+    if greeting.startswith("hello"):
+        return "0"
+    elif greeting.startswith("h"):
+        return "20"
+    else:
+        return "100"
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
